ff3f7227cc16db2b009e6d377706b2213faf7d71
ant @travelmate <anthony.hillairet@gmail.com>
2025-07-13 08:21:19 -0700

    feat: add check-token command

--- cli/__init__.py
+++ cli/__init__.py
@@ -1,4 +1,5 @@
 from .export_issues import export_issues
 from .import_issues import import_issues
+from .check_token import check_token
 
-__all__ = ["export_issues", "import_issues"]
+__all__ = ["export_issues", "import_issues", "check_token"]
